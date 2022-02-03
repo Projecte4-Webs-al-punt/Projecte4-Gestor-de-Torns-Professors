7865f272d34b3e371e3d05a9cdef6b8533147cfa
nemesxv <lupenkov1@gmail.com>
2022-02-03 18:38:56 +0000

    registro y login funcional, header responde correctamente, factory arreglada

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,11 +4,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Register | Tutory</title>
 
-    <script
-            src="https://kit.fontawesome.com/2828f7885a.js"
-            integrity="sha384-WAsFbnLEQcpCk8lM1UTWesAf5rGTCvb2Y+8LvyjAAcxK1c3s5c0L+SYOgxvc6PWG"
-            crossorigin="anonymous"
-    ></script>
+    <script src="https://kit.fontawesome.com/2828f7885a.js"
+        integrity="sha384-WAsFbnLEQcpCk8lM1UTWesAf5rGTCvb2Y+8LvyjAAcxK1c3s5c0L+SYOgxvc6PWG" crossorigin="anonymous">
+    </script>
 </head>
 <section class="hero is-primary is-fullheight">
     <div class="hero-body">
@@ -20,92 +18,97 @@
                             <x-auth-card>
                                 <x-slot name="logo">
                                     <a href="/">
-                                        <x-application-logo/>
+                                        <x-application-logo />
                                     </a>
                                 </x-slot>
 
                                 <!-- Session Status -->
-                                <x-auth-session-status :status="session('status')"/>
+                                <x-auth-session-status :status="session('status')" />
 
                                 <!-- Validation Errors -->
-                                <x-auth-validation-errors class="mb-4" :errors="$errors"/>
+                                <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
                                 <form method="POST" action="{{ route('register') }}">
-                                @csrf
+                                    @csrf
 
-                                <!-- Name -->
+                                    <!-- Name -->
                                     <div>
 
 
                                         <div class="field">
-                                            <x-label for="name" :value="__('Name')"/>
+                                            <x-label for="name" :value="__('Name')" />
                                             <div class="control has-icons-left">
-                                                <x-input class="input" id="email" class="input is-warning" type="email"
-                                                         placeholder="e.g. Kirill Gomez Bourarach"
-                                                         type="text" name="name" :value="old('name')" required
-                                                         autofocus/>
+                                                <x-input class="input" id="name" class="input is-warning"
+                                                    type="name" placeholder="e.g. Benito" type="text" name="name"
+                                                    :value="old('name')" required autofocus />
                                                 <span class="icon is-small is-left">
-                  <i class="fa fa-envelope"></i>
-                </span>
+                                                    <i class="fa fa-user"></i>
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- lastname -->
+                                    <div>
+                                        <div class="field">
+                                            <x-label for="lastname" :value="__('Lastname')" />
+                                            <div class="control has-icons-left">
+                                                <x-input class="input" id="lastname" class="input is-warning"
+                                                    type="lastname" placeholder="e.g. Camelas" type="text"
+                                                    name="lastname" :value="old('lastname')" required autofocus />
+                                                <span class="icon is-small is-left">
+                                                    <i class="fa fa-user"></i>
+                                                </span>
                                             </div>
                                         </div>
                                     </div>
 
                                     <!-- Email Address -->
                                     <div>
-
-
                                         <div class="field">
-                                            <x-label for="email" class="label" :value="__('Email')"/>
+                                            <x-label for="email" class="label" :value="__('Email')" />
                                             <div class="control has-icons-left">
-                                                <x-input class="input" id="email" class="input is-warning" type="email"
-                                                         placeholder="e.g. exemple@cendrassos.net" type="email"
-                                                         name="email" :value="old('email')" required autofocus/>
+                                                <x-input class="input" id="email" class="input is-warning"
+                                                    type="email" placeholder="e.g. exemple@cendrassos.net" type="email"
+                                                    name="email" :value="old('email')" required autofocus />
                                                 <span class="icon is-small is-left">
-                  <i class="fa fa-envelope"></i>
-                </span>
+                                                    <i class="fa fa-envelope"></i>
+                                                </span>
                                             </div>
                                         </div>
                                     </div>
 
                                     <!-- Password -->
                                     <div class="field">
-                                        <x-label for="password" :value="__('Password')" class="label"/>
+                                        <x-label for="password" :value="__('Password')" class="label" />
 
                                         <div class="control has-icons-left">
-                                            <x-input id="password" class="block mt-1 w-full"
-                                                     type="password"
-                                                     name="password"
-                                                     placeholder="*******"
-                                                     class="input is-warning"
-                                                     required autocomplete="current-password"/>
+                                            <x-input id="password" class="block mt-1 w-full" type="password"
+                                                name="password" placeholder="*******" class="input is-warning" required
+                                                autocomplete="current-password" />
                                             <span class="icon is-small is-left">
-                  <i class="fa fa-lock"></i>
-                </span>
+                                                <i class="fa fa-lock"></i>
+                                            </span>
                                         </div>
                                     </div>
 
                                     <!-- Confirm Password -->
                                     <div class="field">
-                                        <x-label for="password_confirmation" :value="__('Confirm Password')"/>
+                                        <x-label for="password_confirmation" :value="__('Confirm Password')" />
 
                                         <div class="control has-icons-left">
                                             <x-input id="password_confirmation" class="block mt-1 w-full"
-                                                     type="password"
-                                                     name="password_confirmation"
-                                                     placeholder="*******"
-                                                     class="input is-warning"
-                                                     required autocomplete="current-password"/>
+                                                type="password" name="password_confirmation" placeholder="*******"
+                                                class="input is-warning" required autocomplete="current-password" />
                                             <span class="icon is-small is-left">
-                  <i class="fa fa-lock"></i>
-                </span>
+                                                <i class="fa fa-lock"></i>
+                                            </span>
                                         </div>
                                     </div>
 
 
                                     <div class="flex items-center justify-end mt-4">
                                         <a class="underline text-sm text-gray-600 hover:text-gray-900"
-                                           href="{{ route('login') }}">
+                                            href="{{ route('login') }}">
                                             {{ __('Already registered?') }}
                                         </a>
 
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,60 +1,78 @@
  <!-- START NAV -->
  <header>
- <nav class="navbar">
-    <div class="container">
-        <div class="navbar-brand">
-            <a class="navbar-item" href="../">
-                <img src="https://i.ibb.co/QnxXzXj/Copia-de-Tutory-cat-1.png" alt="Logo">
-            </a>
-            <span class="navbar-burger burger" data-target="navbarMenu">
-                    <span></span>
-            <span></span>
-            <span></span>
-            </span>
-        </div>
-        <div id="navbarMenu" class="navbar-menu">
-            <div class="navbar-end">
-                <a href="{{ route('home') }}"  class="navbar-item is-active">
-                        Inici
-                    </a>
-                <a href="{{ route('contact') }}" class="navbar-item">
-                        Contacte
-                    </a>
-                <a href="{{ route('about') }}" class="navbar-item">
-                    Sobre nosaltres
-                </a>
-                <div class="navbar-item has-dropdown is-hoverable">
-                    <a class="navbar-link">
-                            Compte
-                        </a>
-                    <div class="navbar-dropdown">
-                        <a href="{{ route('dashboard') }}" class="navbar-item">
-                                Panell de control
-                            </a>
-                        <a class="navbar-item">
-                                Perfil
-                            </a>
-                        <a class="navbar-item">
-                                Parametres
-                            </a>
-                        <hr class="navbar-divider">
-                        <div class="navbar-item">
-                            <form method="POST" action="{{ route('logout') }}">
-                                @csrf
+     <nav class="navbar">
+         <div class="container">
+             <div class="navbar-brand">
+                 <a class="navbar-item" href="../">
+                     <img src="https://i.ibb.co/QnxXzXj/Copia-de-Tutory-cat-1.png" alt="Logo">
+                 </a>
+                 <span class="navbar-burger burger" data-target="navbarMenu">
+                     <span></span>
+                     <span></span>
+                     <span></span>
+                 </span>
+             </div>
+             <div id="navbarMenu" class="navbar-menu">
+                 <div class="navbar-end">
+                     <a href="{{ route('home') }}" class="navbar-item is-active">
+                         Inici
+                     </a>
+                     <a href="{{ route('contact') }}" class="navbar-item">
+                         Contacte
+                     </a>
+                     <a href="{{ route('about') }}" class="navbar-item">
+                         Sobre nosaltres
+                     </a>
+                     @if (Auth::check())
+                         <div class="navbar-item has-dropdown is-hoverable">
+                             <a class="navbar-link">
+                                 <figure class="navbar-end image ">
+                                     <img class="is-rounded is-32x32"
+                                         src="https://bulma.io/images/placeholders/128x128.png">
+                                 </figure>
+                             </a>
+                             <div class="navbar-dropdown">
+                                 @if (Auth::user()->role == 'admin')
+                                     <a href="{{ route('dashboard') }}" class="navbar-item">
+                                         Panell de control
+                                     </a>
+                                 @endif
 
-                                <x-dropdown-link :href="route('logout')"
-                                                 onclick="event.preventDefault();
+                                 <a class="navbar-item">
+                                     Perfil
+                                 </a>
+                                 <a class="navbar-item">
+                                     Parametres
+                                 </a>
+                                 <hr class="navbar-divider">
+                                 <div class="navbar-item">
+                                     <form method="POST" action="{{ route('logout') }}">
+                                         @csrf
+
+                                         <x-dropdown-link :href="route('logout')" onclick="event.preventDefault();
                                                 this.closest('form').submit();">
-                                    {{ __('Log Out') }}
-                                </x-dropdown-link>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</nav>
-</header>
+                                             {{ __('Log Out') }}
+                                         </x-dropdown-link>
+                                     </form>
+                                 </div>
+                             </div>
+                         </div>
+                     @else
+                         <div class="navbar-item">
+                             <a href="{{ route('login') }}" class="navbar-item ">
+                                 Iniciar Sessió  <i class="fa fa-sign-in" ></i>
+                             </a>
+                         </div>
+                         <div class="navbar-item">
+                             <a href="{{ route('register') }}" class="navbar-item">
+                                 Registrar-se <i class="fa fa-user-plus" aria-hidden="true"></i>
+                             </a>
+                         </div>
+                     @endif
+                 </div>
+             </div>
+         </div>
+     </nav>
+ </header>
 
-<!-- END NAV -->
+ <!-- END NAV -->
