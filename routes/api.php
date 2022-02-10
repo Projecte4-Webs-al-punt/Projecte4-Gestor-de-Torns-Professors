fe0ea7c3e119cf3b7c6c9a6857ff6f439bf0789b
juanjogomezvillegas <juanjogomvil@gmail.com>
2022-02-10 19:40:54 +0100

    modals en el crud de usuaris funcionant casi.

--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/users',[UserController::class,'index']);
 
 Route::get('/doubts',[DoubtController::class,'index']);
+
+Route::get('/users/{id}',[UserController::class,'show']);
