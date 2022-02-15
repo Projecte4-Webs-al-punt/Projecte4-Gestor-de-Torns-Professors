363d03c2e251ffa62ddba38589130108b8bb26df
juanjogomezvillegas <juanjogomvil@gmail.com>
2022-02-15 22:47:30 +0100

    intent de crear el primer component amb vue

--- tests/Unit/Dashboard.spec.js
+++ tests/Unit/Dashboard.spec.js
@@ -1,20 +1,10 @@
-import { mount } from '@vue/test-utils'
-import Dashboard from '@/components/Dashboard.vue'
-
+import { mount } from "@vue/test-utils";
+import Dashboard from "../../resources/js/components/Dashboard.vue";
 
 describe('Dashboard.vue Test', () => {
   it('renders message when component is created', () => {
     // render the component
-    const wrapper = mount(Dashboard, {
-      data() {
-        return {
-          countStudent: 0, 
-          countDoubt: 0,
-          countDoubtP: 0, 
-          countDoubtR: 0 
-        }
-      }
-    });
+    const wrapper = mount(Dashboard);
 
     // check the name of the component
     expect(wrapper.vm.$options.name).toMatch('Dashboard');
