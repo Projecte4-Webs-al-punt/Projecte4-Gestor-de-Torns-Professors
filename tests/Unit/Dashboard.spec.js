1175efd699477b8557d4bb1682c4ef49e84e4a65
juanjogomezvillegas <juanjogomvil@gmail.com>
2022-02-15 22:37:53 +0100

    intent de crear el primer component amb vue

--- tests/Unit/Dashboard.spec.js
+++ tests/Unit/Dashboard.spec.js
@@ -1,23 +1,22 @@
-import { shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import Dashboard from '@/components/Dashboard.vue'
 
 
 describe('Dashboard.vue Test', () => {
   it('renders message when component is created', () => {
     // render the component
-    const wrapper = shallowMount(Dashboard, {
-      propsData: {
-        countStudent: 0, 
-        countDoubt: 0,
-        countDoubtP: 0, 
-        countDoubtR: 0 
+    const wrapper = mount(Dashboard, {
+      data() {
+        return {
+          countStudent: 0, 
+          countDoubt: 0,
+          countDoubtP: 0, 
+          countDoubtR: 0 
+        }
       }
-    })
+    });
 
     // check the name of the component
     expect(wrapper.vm.$options.name).toMatch('Dashboard');
-
-    // check that the title is rendered
-    expect(wrapper.text()).toMatch('Total Alumnes');
   });
 });
